69a19b64ccde53e67345bf9e2a9557b879f522e8
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-08-28 21:08:42 +0500

    Update post-featured-card.blade.php

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -41,7 +41,7 @@
                 <div>
                     <a href="/posts/{{$post->slug}}"
                        class="transition-colors duration-300 text-xs font-semibold bg-purple-500 hover:bg-purple-700 rounded-full py-2 px-8 whitespace-nowrap"
-                    >Read More</a>
+                    >Read Mor</a>
                 </div>
             </footer>
         </div>
